27c4e5313d92dc04ec3025c76ef0d245568d9a38
umamaheshballa8 <70004886+umamaheshballa8@users.noreply.github.com>
2021-12-09 17:47:37 +0530

    Update TestBase.java

--- src/main/java/com/rsr/qa/base/TestBase.java
+++ src/main/java/com/rsr/qa/base/TestBase.java
@@ -30,7 +30,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.online.Downloader;
 import com.rsr.qa.utlities.ExcalFileReader;
 import com.rsr.qa.utlities.TestUtil;
 
